565607230b17ae2c5966b8e1ec574d53107a8870
Filip Swierczynski <swirek.gw@hotmail.com>
2023-12-24 11:11:16 -0500

    added task component

--- ang-kanban/src/app/app.component.ts
+++ ang-kanban/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-kanban';
+
+  todo: Task[] = [
+    {
+      title: 'Buy Milk',
+      description: 'Go buy milk'
+    },
+    {
+      title: 'Create a Kanban board',
+      description: 'Using firebase and angular create kanban board'
+    }
+  ]
 }
